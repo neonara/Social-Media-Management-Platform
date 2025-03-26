8661ec9a714d6a1d363607f2e56e57a8cf366b3d
Achref <achref.maarfi0@gmail.com>
2025-03-26 11:48:34 +0100

    update repo

--- social_media_management/settings.py
+++ social_media_management/settings.py
@@ -31,16 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.accounts',         
-    # 'apps.content',           
-    # 'apps.planning',          
-    # 'apps.analytics',        
-    # 'apps.notifications',    
-    # 'apps.ai_integration',   
-    # 'apps.social_media',    
-    # 'apps.collaboration',
-    'rest_framework',
-    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,34 +75,15 @@ WSGI_APPLICATION = 'social_media_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_media_platform',  
-        'USER': 'postgres',       
-        'PASSWORD': 'achref',    
-        'HOST': 'localhost',           
-        'PORT': '5432',           
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-from datetime import timedelta
 
-REST_FRAMEWORK = {
-    'NON_FIELD_ERRORS_KEY': 'error',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=90),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
-}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'accounts.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
