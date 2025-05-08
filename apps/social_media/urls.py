9e02866b5da718712690d4525b420a67ab160d17
Alae Nbiba <dnabiba@gmailcom>
2025-05-08 15:51:33 +0100

    set cache timeout to none

--- apps/social_media/urls.py
+++ apps/social_media/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import publish_to_facebook, publish_to_instagram, publish_to_linkedin, FacebookConnectView, FacebookCallbackView,FacebookDisconnectView
 
 urlpatterns = [
-    path('social/facebook/<int:post_id>/publish/', publish_to_facebook, name='publish_facebook'),
-    path('social/instagram/<int:post_id>/publish/', publish_to_instagram, name='publish_instagram'),
-    path('social/linkedin/<int:post_id>/publish/', publish_to_linkedin, name='publish_linkedin'),
+    path('facebook/<int:post_id>/publish/', publish_to_facebook, name='publish_facebook'),
+    path('instagram/<int:post_id>/publish/', publish_to_instagram, name='publish_instagram'),
+    path('linkedin/<int:post_id>/publish/', publish_to_linkedin, name='publish_linkedin'),
 
-    path('social/facebook/connect/', FacebookConnectView.as_view(), name='facebook_connect'),
-    path('social/facebook/callback/', FacebookCallbackView.as_view(), name='facebook_callback'),
-    path('social/facebook/disconnect/', FacebookDisconnectView.as_view(), name='facebook_disconnect'),
+    path('facebook/connect/', FacebookConnectView.as_view(), name='facebook_connect'),
+    path('facebook/callback/', FacebookCallbackView.as_view(), name='facebook_callback'),
+    path('facebook/disconnect/', FacebookDisconnectView.as_view(), name='facebook_disconnect'),
 ]
--- social_media_management/settings.py
+++ social_media_management/settings.py
@@ -29,7 +29,8 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
+        "TIMEOUT": None, # Set to None for no timeout
     }
 }
 
