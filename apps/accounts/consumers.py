291be5c89344f8eed44a9182f4cf1b5017162858
achref <achref.maarfi0@gmail.com>
2025-06-16 13:26:56 +0100

    active users bubbles set with websocket

--- apps/accounts/consumers.py
+++ apps/accounts/consumers.py
@@ -1,5 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from asgiref.sync import async_to_sync
 
 class MyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,3 +13,76 @@ class MyConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         await self.send(text_data=json.dumps({"message": f"Received: {data}"}))
+
+class UserActivityConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = "user_activity"
+        self.user_id = self.scope["user"].id if self.scope["user"].is_authenticated else None
+
+        if self.user_id:
+            # Add user to the group
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+
+            # Notify group about the new user
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    "type": "user_joined",
+                    "user": {
+                        "id": self.user_id,
+                        "name": f"{self.scope['user'].first_name} {self.scope['user'].last_name}".strip() if self.scope['user'].first_name or self.scope['user'].last_name else self.scope['user'].email,
+                        "email": self.scope["user"].email,
+                        "profilePicture": self.scope["user"].user_image.url if self.scope["user"].user_image else ""
+                    }
+                }
+            )
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        if self.user_id:
+            # Remove user from the group
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
+
+            # Notify group about the user leaving
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    "type": "user_left",
+                    "userId": self.user_id
+                }
+            )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        # Broadcast the received data to the group, excluding the sender
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "user_activity_message",
+                "message": data,
+                "sender_channel_name": self.channel_name
+            }
+        )
+
+    async def user_joined(self, event):
+        # Send the user joined message to WebSocket, excluding the current user
+        if self.user_id != event["user"]["id"]:
+            await self.send(text_data=json.dumps({"type": "user-joined", "user": event["user"]}))
+
+    async def user_left(self, event):
+        # Send the user left message to WebSocket, excluding the current user
+        if self.user_id != event["userId"]:
+            await self.send(text_data=json.dumps({"type": "user-left", "userId": event["userId"]}))
+
+    async def user_activity_message(self, event):
+        # Exclude the sender from receiving their own message
+        if self.channel_name != event.get("sender_channel_name"):
+            await self.send(text_data=json.dumps(event["message"]))
--- social_media_management/asgi.py
+++ social_media_management/asgi.py
@@ -12,12 +12,14 @@ from social_media_management.websocket_auth import TokenAuthMiddlewareStack
 # Import routing configurations from all apps
 import apps.notifications.routing
 import apps.collaboration.routing
+from apps.accounts.routing import websocket_urlpatterns as accounts_websocket_urlpatterns
 
 
 # Combine WebSocket URL patterns from all apps
 all_websocket_urlpatterns = (
     apps.notifications.routing.websocket_urlpatterns +
-    apps.collaboration.routing.websocket_urlpatterns
+    apps.collaboration.routing.websocket_urlpatterns +
+    accounts_websocket_urlpatterns
 )
 print("ASGI setup - loading application")
 
@@ -25,7 +27,9 @@ print("TokenAuthMiddleware activated")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": TokenAuthMiddlewareStack(
-        URLRouter(all_websocket_urlpatterns)
-    )
+    "websocket": AllowedHostsOriginValidator(
+        TokenAuthMiddlewareStack(
+            URLRouter(all_websocket_urlpatterns)
+        )
+    ),
 })
